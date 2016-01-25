aad1675a50c37e1c827fb7396f8e8e28f63ba3d6
Chobits <chobits@live.cn>
2016-01-25 22:35:17 +0800

    add vue file to dist

---
+++ dist/vue-pagination.vue
@@ -0,0 +1,154 @@
+﻿<template>
+    <nav v-show="totalPages > 1">
+        <ul class="pagination">
+            <li>
+                <a style="cursor:pointer;" v-if="!noPrevious" v-on:click="selectPage(pageNo - 1)" aria-label="Previous">
+                    <span aria-hidden="true">‹</span>
+                </a>
+            </li>
+            <li v-for="page in pages" v-bind:class="{ \'active\': page.number == pageNo, \'disabled\': page.disabled }">
+                <a style="cursor:pointer;" v-on:click="selectPage(page.number)" v-text="page.text"></a>
+            </li>
+            <li>
+                <a style="cursor:pointer;" v-if="!noNext" v-on:click="selectPage(pageNo + 1)" aria-label="Next">
+                    <span aria-hidden="true">›</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+
+    module.exports = {
+        props: {
+            pageNo: {
+                type: Number,
+                validator: function (value) {
+                    return value > 0
+                },
+                default: 1
+            },
+            pageSize: {
+                type: Number,
+                validator: function (value) {
+                    return value > 0
+                },
+                default: 10
+            },
+            totalResult: {
+                type: Number,
+                default: 0
+            },
+            displayNum: {
+                type: Number,
+                default: 6
+            },
+            edgeNum: {
+                type: Number,
+                default: 2
+            },
+        },
+        replace: true,
+        inherit: false,
+        computed: {
+            noPrevious: function () {
+                return this.pageNo === 1;
+            },
+            noNext: function () {
+                return this.pageNo === this.totalPages;
+            },
+            pages: function () {
+                return getPages(this.pageNo, this.totalPages, this.edgeNum, this.displayNum);
+            },
+            totalPages: function () {
+                return getTotalPages(this.pageSize, this.totalResult);
+            },
+        },
+        methods: {
+            selectPage: function (num) {
+                if (this.pageNo != num && num > 0 && num <= this.totalPages) {
+                    this.$dispatch('page-change', num);
+                }
+            },
+            selectSize: function (size) {
+                if (this.pageSize != size && size > 0) {
+                    var totalPages = getTotalPages(size, this.totalResult);
+                    if (this.pageNo <= totalPages) {
+                        this.$dispatch('size-change', size);
+                    }
+                }
+            },
+        },
+    };
+
+    function getTotalPages(pageSize, totalResult) {
+        var totalPages = pageSize < 1 ? 1 : Math.ceil(totalResult / pageSize);
+        return Math.max(totalPages || 0, 1);
+    }
+
+    // Create page object used in template
+    function makePage(number, text, isActive) {
+        return {
+            number: number,
+            text: text,
+            disabled: number === -1,
+        };
+    }
+
+    /**
+    * Calculate start and end point of pagination links depending on
+    * currentPage and num_display_entries.
+    * @return {Array}
+    */
+    function getInterval(currentPage, pageCount, num_display_entries) {
+        //var num_display_entries = 6;
+        var ne_half = Math.ceil(num_display_entries / 2);
+        var np = pageCount;
+        var upper_limit = np - num_display_entries;
+        var start = currentPage > ne_half ? Math.max(Math.min(currentPage - ne_half, upper_limit), 0) : 0;
+        var end = currentPage > ne_half ? Math.min(currentPage + ne_half, np) : Math.min(num_display_entries, np);
+        return [start, end];
+    }
+
+    function getPages(currentPage, totalPages, num_edge_entries, num_display_entries) {
+        var ret = [];
+        //var num_edge_entries = 2;
+        var skip_text = '...';
+        var np = totalPages;
+        var interval = getInterval(currentPage - 1, totalPages, num_display_entries);
+
+        // Generate starting points
+        if (interval[0] > 0 && num_edge_entries > 0) {
+            var end = Math.min(num_edge_entries, interval[0]);
+            for (var i = 0; i < end; i++) {
+                var page = makePage(i + 1, i + 1);
+                ret.push(page);
+            }
+            if (num_edge_entries < interval[0]) {
+                var page = makePage(-1, skip_text);
+                ret.push(page);
+            }
+        }
+        // Generate interval links
+        for (var i = interval[0]; i < interval[1]; i++) {
+            var page = makePage(i + 1, i + 1);
+            ret.push(page);
+        }
+        // Generate ending points
+        if (interval[1] < np && num_edge_entries > 0) {
+            if (np - num_edge_entries > interval[1]) {
+                var page = makePage(-1, skip_text);
+                ret.push(page);
+            }
+            var begin = Math.max(np - num_edge_entries, interval[1]);
+            for (var i = begin; i < np; i++) {
+                var page = makePage(i + 1, i + 1);
+                ret.push(page);
+            }
+        }
+
+        return ret;
+    }
+
+</script>
